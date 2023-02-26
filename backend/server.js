00098e71f33b73a27802cdf49af22312de58411a
Hassan-Bin-Tariq <58106042+Ibnet-pixel@users.noreply.github.com>
2023-02-26 20:33:26 +0500

    objectSetter Function

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,10 @@ dotenv.config({ path: "./backend/config/config.env" })
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
 });
+
+function objectSetter()
+{
+    console.log(working)
+    var first;
+    const user = "Ali@gmail.com"  //hardcoding the user
+}
